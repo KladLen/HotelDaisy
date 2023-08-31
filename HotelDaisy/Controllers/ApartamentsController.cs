a421d93399466a5260956122bc7fe87c1d8fe13d
Klaudia Lenarcik <klalen438@gmail.com>
2023-08-31 21:20:41 +0200

    Controller and View for Apartaments

---
+++ HotelDaisy/Controllers/ApartamentsController.cs
@@ -0,0 +1,20 @@
+﻿using HotelDaisy.Data;
+using HotelDaisy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelDaisy.Controllers
+{
+	public class ApartamentsController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public ApartamentsController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			IEnumerable<Apartament> objApartamentList = _db.Apartaments;
+			return View(objApartamentList);
+		}
+	}
+}
