1f5361fbb5f13a6806b81be82d5b72bb811061c7
Klaudia Lenarcik <klalen438@gmail.com>
2023-09-23 16:35:19 +0200

    View and ation for creating reservation with selected apartment

--- HotelDaisy/Controllers/ReservationController.cs
+++ HotelDaisy/Controllers/ReservationController.cs
@@ -24,6 +24,7 @@ namespace HotelDaisy.Controllers
 		{
 			return View();
 		}
+
 		//POST
 		[HttpPost]
 		public IActionResult Index(ReservationTime obj)
@@ -125,5 +126,72 @@ namespace HotelDaisy.Controllers
 
             return View();
 		}
-	}
+
+        //GET
+        public IActionResult CreateForSelectedApartment(int id)
+        {
+     //       TempData["Id"] = id;
+            var viewModel = new ReservationTimeForOneApartment
+            {
+                ApartmentId = id,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddDays(1)
+            };
+
+            return View(viewModel);
+        }
+
+        //POST
+        [HttpPost, Authorize]
+        public IActionResult CreateForSelectedApartment(ReservationTimeForOneApartment obj)
+        {
+            var viewModel = new ReservationTimeForOneApartment
+            {
+                ApartmentId = obj.ApartmentId,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddDays(1)
+            };
+            if (ModelState.IsValid)
+            {
+                if (User.Identity.IsAuthenticated)
+                {
+                    if (obj.StartDate >= obj.EndDate)
+                    {
+                        ModelState.AddModelError("", "Start date must be earlier than end date.");
+                        return View(viewModel);
+                    }
+
+                    if (obj.StartDate < DateTime.Now)
+                    {
+                        ModelState.AddModelError("", "The date can't be earlier than today's date.");
+                        return View(viewModel);
+                    }
+
+                    //var id = (int)TempData["Id"];
+                    var isAvailable = _db.Reservations
+                        .Where(r => r.ApartmentId == obj.ApartmentId)
+                        .All(o => (obj.StartDate <= o.StartDate && obj.EndDate <= o.StartDate) || (obj.StartDate >= o.EndDate && obj.EndDate >= o.EndDate));
+                    if (isAvailable)
+                    {
+                        Reservation reservation = new Reservation
+                        {
+                            StartDate = obj.StartDate,
+                            EndDate = obj.EndDate,
+                            UserId = _userManager.GetUserId(User),
+                            ApartmentId = obj.ApartmentId
+                        };
+                        _db.Reservations.Add(reservation);
+                        _db.SaveChanges();
+                        return View("Index");
+                    }
+
+                    ModelState.AddModelError("", "Selected Apartment is not available at this time. Check another dates.");
+                    return View(viewModel);
+                }
+            }
+
+            ModelState.AddModelError("", "Input date are not valid.");
+            return View(viewModel);
+        }
+    }
 }
---
+++ HotelDaisy/Models/ViewModels/ReservationTimeForOneApartment.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace HotelDaisy.Models.ViewModels
+{
+    public class ReservationTimeForOneApartment
+    {
+        [DisplayName("Start Date")]
+        public DateTime StartDate { get; set; }
+        [DisplayName("End Date")]
+        public DateTime EndDate { get; set; }
+        public int ApartmentId { get; set; }
+    }
+}
